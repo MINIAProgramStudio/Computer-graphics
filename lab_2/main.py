1ef65cafc28c71707f6130ea53169217f3236f1c
Mykhailo Holub <mishataube2005@gmail.com>
2024-03-08 12:02:52 +0200

    tasks 1-3 done

--- lab_2/main.py
+++ lab_2/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import shape_handler
-
+# task 1
 cube_shape = pd.DataFrame([
     [0,0,0],
     [0,0,1],
@@ -22,3 +22,25 @@ cube_big = cube_shape.transform([
     [0,0,2,0]
 ])
 cube_big.draw()
+
+cube_small = cube_shape.transform([
+    [0.5,0,0,0],
+    [0,0.5,0,0],
+    [0,0,0.5,0]
+])
+cube_small.draw()
+# task 2
+cube_big_mirrored_zero = cube_big.transform([
+    [-1,0,0,0],
+    [0,-1,0,0],
+    [0,0,-1,0]
+])
+cube_big_mirrored_zero.draw()
+
+cube_big_mirrored_X = cube_big.transform([
+    [-1,0,0,0],
+    [0,1,0,0],
+    [0,0,1,0]
+])
+cube_big_mirrored_X.draw()
+# task 4
