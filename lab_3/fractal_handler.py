3096a97d93470c3e0432eb370d5295737869edc9
Mykhailo Holub <mishataube2005@gmail.com>
2024-04-24 12:43:35 +0300

    aaaaaaaaaa

--- lab_3/fractal_handler.py
+++ lab_3/fractal_handler.py
@@ -1,60 +1,76 @@
 import turtle
 import json
 import os
+import io
+import PIL
+from tkinter import *
 
-if os.path.isfile("memory.json"):
-    file = open("memory.json","r")
+if not os.path.isdir("data"):
+    os.mkdir("data")
+
+if os.path.isfile("data/memory.json"):
+    file = open("data/memory.json","r+")
     memory = json.loads(file.read())
-    print("memory loaded")
     file.close()
     del(file)
 else:
-    file = open("memory.json","x")
+    file = open("data/memory.json","x")
     file.close()
     del(file)
-    memory = {}
+    memory = {"counter": 0}
 
 turtle.setup(1000, 1000, 10, 10)
 def draw_l_fractal(l_string,step,angle, name = ""):
-    turtle.speed(0)
-    turtle.hideturtle()
-    turtle.tracer(False)
-    turtle.pendown()
-    turtle.title(name)
-    turtle.setworldcoordinates(-500,-500,1000,1000)
-    for character in l_string:
-        match(character):
-            case "F":
-                turtle.forward(step)
-            case "+":
-                turtle.left(angle)
-            case "-":
-                turtle.right(angle)
-            case _:
-                pass
-    turtle.update()
-    input()
-    turtle.reset()
-
-def iterate(l_string, dictionary):
-    if str(l_string)+str(dictionary) in memory.keys():
-        return memory[str(l_string)+str(dictionary)]
+    key = str(l_string)+";"+str(step)+";"+str(angle)
+    if key in memory.keys():
+        print("loaded")
+        file_name = memory[key]
+        if os.path.isfile("data/"+file_name):
+            turtle.bgpic("data/"+file_name)
+            input()
     else:
-        keys = dictionary.keys()
-        new_l_string = ""
+        turtle.speed(0)
+        turtle.hideturtle()
+        turtle.tracer(False)
+        turtle.pendown()
+        turtle.title(name)
+        turtle.setworldcoordinates(-500,-500,1000,1000)
         for character in l_string:
-            if character in keys:
-                new_l_string += dictionary[character]
-            else:
-                new_l_string += character
-        memory[str(l_string)+str(dictionary)] = new_l_string
-        file = open("memory.json","w")
-        file.write(json.dumps(memory))
+            match(character):
+                case "F":
+                    turtle.forward(step)
+                case "+":
+                    turtle.left(angle)
+                case "-":
+                    turtle.right(angle)
+                case _:
+                    pass
+        turtle.update()
+        turtle_screen = turtle.getscreen()
+        memory["counter"]+=1
+        file_name = str(memory["counter"])+".ps"
+        memory[key]=file_name
+        turtle_screen.getcanvas().postscript(file="data/"+file_name)
+        file = open("data/memory.json","w")
+        file.write(json.dumps(memory, sort_keys=True))
         file.close()
         del(file)
-        return new_l_string
+        input()
+        turtle.reset()
+
+def iterate(l_string, dictionary):
+    keys = dictionary.keys()
+    new_l_string = ""
+    for character in l_string:
+        if character in keys:
+            new_l_string += dictionary[character]
+        else:
+            new_l_string += character
+    return new_l_string
 
 def draw_nth_l_fractal(l_string, dictionary, step, angle, power = 0, name = ""):
+    print("calculating")
     for i in range(power):
         l_string = iterate(l_string,dictionary)
+    print("drawing")
     draw_l_fractal(l_string,step,angle,name)
--- lab_3/main.py
+++ lab_3/main.py
@@ -1,25 +1,24 @@
 import fractal_handler as fh
-import sys
 
-for i in range(8):
+for i in range(10):
     fh.draw_nth_l_fractal("F++F++F", {
         "F":"F-F++F-F"
     }, 200/(i**3+1),60,i,"Сніжинка Коха")
 
-for i in range(6):
+for i in range(8):
     fh.draw_nth_l_fractal("F+XF+F+XF", {
         "X":"XF-F+F-XF+F+XF-F+F-X"
     }, 200/(i**2.2+1),90,i,"Хрестоподібний фрактал")
 
-for i in range(6):
+for i in range(8):
     fh.draw_nth_l_fractal("F+F+F+F", {
         "F":"FF+F++F+F"
     }, 200/(i**3+1),90,i,"Кристалоподібний фрактал")
 
-for i in range(4):
+for i in range(1,6):
     fh.draw_nth_l_fractal("X", {
         "X":"XFYFX+F+YFXFY-F-XFYFX",
         "Y":"YFXFY-F-XFYFX+F+YFXFY"
-    }, 200/(i**2.5+1),90,i,"Фрактальна крива Пеано")
+    }, 200/(i**3+1),90,i,"Фрактальна крива Пеано")
 
 print(sys.getsizeof(fh.memory))
