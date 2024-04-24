ece7c4dd296a4d69f95976f491c180ab744abbb4
Mykhailo Holub <mishataube2005@gmail.com>
2024-04-24 21:06:51 +0300

    Lab done

--- lab_3/fractal_handler.py
+++ lab_3/fractal_handler.py
@@ -1,9 +1,10 @@
 import turtle
+import time
 
 turtle.setup(1000, 1000, 10, 10)
 
 
-def draw_l_fractal(l_string, step, angle, name=""):
+def draw_l_fractal(l_string, step, angle, name="",t_reset = True, wait = True):
     turtle.speed(0)
     turtle.hideturtle()
     turtle.tracer(False)
@@ -21,6 +22,23 @@ def draw_l_fractal(l_string, step, angle, name=""):
             case _:
                 pass
     turtle.update()
+    if wait:
+        input()
+    if t_reset:
+
+        turtle.reset()
+
+def animate(l_string, step, angle_min, angle_max, angle_step = 1, sleep = 1, name = ""):
+    angle = angle_min
+    while angle<=angle_max:
+        start = time.time()
+        turtle.reset()
+        draw_l_fractal(l_string,step, angle, name = name, t_reset=False, wait = False)
+        turtle.update()
+        sleep_time = sleep - time.time() + start
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+        angle+=angle_step
     input()
     turtle.reset()
 
@@ -37,8 +55,11 @@ def iterate(l_string, dictionary):
 
 
 def draw_nth_l_fractal(l_string, dictionary, step, angle, power=0, name=""):
-    print("calculating")
     for i in range(power):
         l_string = iterate(l_string, dictionary)
-    print("drawing")
     draw_l_fractal(l_string, step, angle, name)
+
+def animate_nth_fractal(l_string,dictionary, step, angle_min, angle_max, power = 0, angle_step = 1, sleep = 1, name = ""):
+    for i in range(power):
+        l_string = iterate(l_string, dictionary)
+    animate(l_string,step,angle_min,angle_max,angle_step,sleep,name)
--- lab_3/main.py
+++ lab_3/main.py
@@ -1,6 +1,6 @@
 import fractal_handler as fh
-
-"""
+# task 3
+print("task 3")
 for i in range(8):
     fh.draw_nth_l_fractal("F++F++F", {
         "F":"F-F++F-F"
@@ -37,4 +37,59 @@ for i in range(7):
     fh.draw_nth_l_fractal("F++F++F++F++F", {
         "F": "F++F++F+++++F-F++F"
     }, 200 / (i ** 2.7 + 1), 36, i, "П'ятикутна фрактальна сніжинка")
-"""
+
+for i in range(5):
+    fh.draw_nth_l_fractal("XF", {
+        "X": "X+YF++YF-FX--FXFX-YF+",
+        "Y": "-FX+YFYF++YF+FX--FX-Y"
+    }, 200 / (i ** 2.2 + 1), 60, i, "Гексагональна крива Госпера")
+
+# task 4
+print("task 4")
+for i in range(5):
+    fh.draw_nth_l_fractal("F++F++F", {
+        "F":"F-F++F-F"
+    }, 200/(i**3+1),30,i,"Сніжинка Коха з кутом 30")
+
+for i in range(8):
+    fh.draw_nth_l_fractal("F+XF+F+XF", {
+        "X":"XF-F+F-XF+F+XF-F+F-X"
+    }, 200/(i+1),120,i,"Хрестоподібний фрактал з кутом 120")
+
+for i in range(5):
+    fh.draw_nth_l_fractal("F+F+F+F", {
+        "F":"FF+F++F+F"
+    }, 200/(i**2+1),120,i,"Кристалоподібний фрактал з кутом 120")
+
+for i in range(1,5):
+    fh.draw_nth_l_fractal("X", {
+        "X":"XFYFX+F+YFXFY-F-XFYFX",
+        "Y":"YFXFY-F-XFYFX+F+YFXFY"
+    }, 200/(5*i**2.5+1),45,i,"Фрактальна крива Пеано з кутом 45")
+
+for i in range(5):
+    fh.draw_nth_l_fractal("F+F+F+F", {
+        "F": "FF+F+F+F+FF"
+    }, 200 / (i ** 2 + 1), 120, i, "Фрактальна дошка з кутом 120")
+
+for i in range(10):
+    fh.draw_nth_l_fractal("YF", {
+        "X": "YF+XF+Y",
+        "Y": "XF-YF-X"
+    }, 200 / (i ** 1.5 + 1), 90, i, "Наконечник стріли Серпінського з кутом 90")
+
+for i in range(5):
+    fh.draw_nth_l_fractal("F++F++F++F++F", {
+        "F": "F++F++F+++++F-F++F"
+    }, 200 / (i ** 2 + 1), 48, i, "П'ятикутна фрактальна сніжинка з кутом 48")
+
+for i in range(5):
+    fh.draw_nth_l_fractal("XF", {
+        "X": "X+YF++YF-FX--FXFX-YF+",
+        "Y": "-FX+YFYF++YF+FX--FX-Y"
+    }, 200 / (i ** 3 + 1), 30, i, "Гексагональна крива Госпера з кутом 30")
+# task 5
+print("task 5")
+fh.animate_nth_fractal("F++F++F",{
+        "F":"F-F++F-F"
+    }, 200/(5**3+1),30,60,5,0.1,0.1,"Анімована сніжинка Коха")
