e849db4f377bace8cd02473a6784bcf780153637
Mykhailo Holub <mishataube2005@gmail.com>
2024-04-27 19:45:16 +0300

    a

--- lab_4/handler.py
+++ lab_4/handler.py
@@ -1,18 +1,74 @@
 import turtle
+import copy
 
-def draw_rectangle(pos_x, pos_y, rotation, size_x, size_y, step = 10):
+depth = 0
+
+def draw_rectangle(pos_x, pos_y, rotation, size_x, size_y, step=10):
     turtle.speed(0)
     turtle.hideturtle()
     turtle.tracer(False)
     turtle.penup()
-    turtle.goto(pos_x*step,pos_y*step)
+    turtle.goto(pos_x * step, pos_y * step)
+    turtle.setheading(rotation)
+    turtle.back(size_x * step / 2)
+    turtle.left(90)
+    turtle.forward(size_y * step / 2)
     turtle.pendown()
     turtle.setheading(rotation)
-    turtle.forward(size_x*step)
+    turtle.forward(size_x * step)
     turtle.right(90)
-    turtle.forward(size_y*step)
+    turtle.forward(size_y * step)
     turtle.right(90)
     turtle.forward(size_x * step)
     turtle.right(90)
     turtle.forward(size_y * step)
     turtle.update()
+
+
+class Rectangle():
+    def __init__(self, pos_x, pos_y, rotation, size_x, size_y):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.rotation = rotation
+        self.size_x = size_x
+        self.size_y = size_y
+
+
+class RectangleGroup():
+    def __init__(self, pos_x, pos_y, rotation, size_x, size_y, contains=[]):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.rotation = rotation
+        self.size_x = size_x
+        self.size_y = size_y
+        self.contains = contains
+
+    def draw(self, step, pos_x=0, pos_y=0, rotation=0, size_x=1, size_y=1):
+        for element in self.contains:
+
+            if isinstance(element, RectangleGroup):
+                global depth
+                print(depth)
+                depth += 1
+                element.draw(step, self.pos_x + pos_x * size_x, self.pos_y + pos_y*size_y, self.rotation * rotation,
+                             self.size_x * size_x, self.size_y)
+            else:
+                draw_rectangle(pos_x + element.pos_x * self.size_x, pos_y + element.pos_y * self.size_y,
+                               self.rotation + element.rotation, self.size_x * element.size_x,
+                               self.size_y * element.size_y, step)
+
+    def iterate(self, generator):
+        for i in range(len(self.contains)):
+            if isinstance(self.contains[i],RectangleGroup):
+                global depth
+                print(depth)
+                depth += 1
+                self.contains[i].iterate(generator)
+            else:
+                rect = copy.deepcopy(self.contains[i])
+                self.contains[i] = copy.deepcopy(generator)
+                self.contains[i].pos_x += rect.pos_x
+                self.contains[i].pos_y += rect.pos_y
+                self.contains[i].size_x *= rect.size_x
+                self.contains[i].size_y *= rect.size_y
+                self.contains[i].rotation += rect.rotation
--- lab_4/main.py
+++ lab_4/main.py
@@ -1,4 +1,21 @@
 import handler as h
 
-h.draw_rectangle(10,10,45,10,10)
+square = h.Rectangle(0,0,0,1,1)
+rectangle_1 = h.Rectangle(-0.5,0.5,-35, 1, 0.5)
+rectangle_2 = h.Rectangle(0.5,0.5,30,1.5,0.5)
+
+generator = h.RectangleGroup(0,0,0,1,1, [
+    square,
+    rectangle_1,
+    rectangle_2
+])
+square_group = h.RectangleGroup(0,0,0,1,1, [
+    square
+])
+
+square_group.draw(step = 100)
 input()
+for i in range(5):
+    square_group.iterate(generator)
+    square_group.draw(step = 100)
+    input()
