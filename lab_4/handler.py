1fc3dc7d445cfe2cfa31992580e92fba8ce20bbc
Mykhailo Holub <mishataube2005@gmail.com>
2024-04-27 21:02:14 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- lab_4/handler.py
+++ lab_4/handler.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import importlib
 import matplotlib.pyplot as plt
 
 class Fractal:
@@ -10,10 +11,16 @@ class Fractal:
     y = [0]
     def iterate(self, times):
         for i in range(times):
-            set = self.sets[int(random.random()*len(self.sets))]
+            set_r = random.random()
+            set_pos = -1
+            while set_r > 0:
+                set_pos+=1
+                set_r -= self.sets[set_pos]["p"]
+            set = self.sets[set_pos]
             self.x.append(self.x[-1]*set["a"] + self.y[-1]*set["b"] + set["e"])
             self.y.append(self.x[-1]*set["c"] + self.y[-1]*set["d"] + set["f"])
 
     def draw(self):
+        importlib.reload(plt)
         plt.scatter(self.x, self.y, s = 0.1)
         plt.show()
