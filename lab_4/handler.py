1efa79e862c2288d23aeb27b86ed0ecc2866d2f1
Mykhailo Holub <mishataube2005@gmail.com>
2024-05-01 23:46:36 +0300

    =_=

--- lab_4/handler.py
+++ lab_4/handler.py
@@ -1,5 +1,6 @@
 import random
 import matplotlib.pyplot as plt
+import matplotlib.animation as anime
 import math
 
 class Fractal:
@@ -7,6 +8,7 @@ class Fractal:
         self.sets = sets
         self.x = [0]
         self.y = [0]
+        self.animation = {"counter": 0, "wait":0, "steps":0}
     def iterate(self, times):
         for i in range(times):
             set_r = random.random()
@@ -35,5 +37,27 @@ class Fractal:
     def draw(self,min_dot = 0):
         fig, ax = plt.subplots()
         ax.scatter(self.x[min_dot:], self.y[min_dot:], s = 0.025)
+        #plt.show()
+        plt.close(fig)
+
+    def animate(self, wait, steps):
+        fig, ax = plt.subplots()
+        x = []
+        y = []
+        def frame(frame):
+            ax.clear()
+            if wait != self.animation["wait"] or steps != self.animation["steps"] or frame == 0:
+                self.animation["counter"] = 0
+                self.animation["wait"] = wait
+                self.animation["steps"] = steps
+            window = len(self.x) // steps
+            self.animation["counter"] += 1
+            self.animation["counter"] %= steps
+            x = self.x[:self.animation["counter"] * window]
+            y = self.y[:self.animation["counter"] * window]
+            ax.scatter(x,y,s = 0.025)
+        an = anime.FuncAnimation(fig, frame, interval = wait,frames = steps)
         plt.show()
         plt.close(fig)
+
+
