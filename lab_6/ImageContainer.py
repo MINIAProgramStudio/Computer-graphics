74dfc74f8f1071644ff2b56e17f7d95faa43e232
Mykhailo Holub <mishataube2005@gmail.com>
2024-06-09 20:24:45 +0300

    lab done

--- lab_6/ImageContainer.py
+++ lab_6/ImageContainer.py
@@ -2,54 +2,60 @@ import numpy
 import cv2
 import copy
 
+
+
 class ImageContainer:
-    def __init__(self,path=None):
-        if path != None: #if path is defined, import the image
+    def __init__(self, path=None):
+        if path != None:  # if path is defined, import the image
             self.path = path
             self.data = cv2.imread(path)
+
     path = None
     data = []
 
-    def show(self): #show the image and wait for anykey
+    def show(self):  # show the image and wait for anykey
         cv2.imshow(self.path, self.data)
         cv2.waitKey(0)
 
     def save(self):
         cv2.imwrite(self.path, self.data)
 
-def IC_to_monochrome(input_IC, red = 0.36, green = 0.53, blue = 0.11): #create monochromatic copy of an image
-    if type(input_IC.data[0][0]) != list or not isinstance(input_IC,ImageContainer):
+
+def IC_to_monochrome(input_IC, red=0.36, green=0.53, blue=0.11):  # create monochromatic copy of an image
+    if type(input_IC.data[0][0]) != list or not isinstance(input_IC, ImageContainer):
         Exception("IC_to_monochrome accepts only rgb image containers")
-    output_IC=copy.deepcopy(input_IC) #create copy of an image
-    output_IC.path=None #delete a path of a copy
+    output_IC = copy.deepcopy(input_IC)  # create copy of an image
+    output_IC.path = None  # delete a path of a copy
 
     pos_y = 0
     for row in output_IC.data:
         pos_x = 0
         for pixel in row:
-            output_IC.data[pos_y][pos_x] = int(pixel[0]*red+pixel[1]*green+pixel[2]*blue) #monochromatic transformation
-            pos_x+=1
-        pos_y+=1
+            output_IC.data[pos_y][pos_x] = int(
+                pixel[0] * red + pixel[1] * green + pixel[2] * blue)  # monochromatic transformation
+            pos_x += 1
+        pos_y += 1
 
-    return output_IC #return a monochromatic copy
+    return output_IC  # return a monochromatic copy
+
 
 def monochrome_to_binary(input_IC):
-    if type(input_IC.data[0][0])!=int or not isinstance(input_IC,ImageContainer):
+    if type(input_IC.data[0][0]) != int or not isinstance(input_IC, ImageContainer):
         Exception("monochrome_to_binary accepts only monochromatic image containers")
     output_IC = copy.deepcopy(input_IC)  # create copy of an image
     output_IC.path = None  # delete a path of a copy
 
     if True:
-        brightness_graph = [0]*256
+        brightness_graph = [0] * 256
         for row in input_IC.data:
             for pixel in row:
-                brightness_graph[pixel[0]] +=1
+                brightness_graph[pixel[0]] += 1
 
         total_brightness = sum(brightness_graph)
 
-        brightness_possibility = [0]*256
-        for i in range(0,255):
-            brightness_possibility[i] = brightness_graph[i]/total_brightness
+        brightness_possibility = [0] * 256
+        for i in range(0, 255):
+            brightness_possibility[i] = brightness_graph[i] / total_brightness
 
         def q1(t):
             output = sum(brightness_possibility[0:t])
@@ -59,7 +65,7 @@ def monochrome_to_binary(input_IC):
                 return 0.0001
 
         def q2(t):
-            output = sum(brightness_possibility[t+1:])
+            output = sum(brightness_possibility[t + 1:])
             if output != 0:
                 return output
             else:
@@ -73,16 +79,16 @@ def monochrome_to_binary(input_IC):
 
         def mu2(t):
             output = 0
-            for i in range(t+1,255):
-                output += i*brightness_possibility[i]/q2(t)
+            for i in range(t + 1, 255):
+                output += i * brightness_possibility[i] / q2(t)
             return output
 
         def main_disp(t):
-            return q1(t)*q2(t)*((mu1(t)*mu2(t))**2)
+            return q1(t) * q2(t) * ((mu1(t) * mu2(t)) ** 2)
 
-        max_val = -10**10
+        max_val = -10 ** 10
         t_max = 0
-        for t in range(0,255):
+        for t in range(0, 255):
             value = main_disp(t)
             if value > max_val:
                 t_max = t
@@ -91,7 +97,7 @@ def monochrome_to_binary(input_IC):
         for row in output_IC.data:
             pos_x = 0
             for pixel in row:
-                if pixel[0]>t_max:
+                if pixel[0] > t_max:
                     output_IC.data[pos_y][pos_x] = 255
                 else:
                     output_IC.data[pos_y][pos_x] = 0
@@ -100,22 +106,24 @@ def monochrome_to_binary(input_IC):
 
         return output_IC
 
-def monochrome_binary_slice(input_IC,  channel = 2, bit = 1,):
+
+def monochrome_binary_slice(input_IC, channel=2, bit=1, ):
     output_IC = copy.deepcopy(input_IC)
     pos_y = 0
     for row in input_IC.data:
         pos_x = 0
         for pixel in row:
-            value = int((pixel[channel]%(2**bit))/(2**(bit-1)))
+            value = int((pixel[channel] % (2 ** bit)) / (2 ** (bit - 1)))
             if value != 0:
                 output_IC.data[pos_y][pos_x] = 255
             else:
                 output_IC.data[pos_y][pos_x] = 0
-            pos_x+=1
-        pos_y+=1
+            pos_x += 1
+        pos_y += 1
     return output_IC
 
-def built_in_image(input_IC, binary_IC, channel = 2, bit = 1):
+
+def built_in_image(input_IC, binary_IC, channel=2, bit=1):
     output_IC = copy.deepcopy(input_IC)
     binary_height = len(binary_IC.data)
     binary_width = len(binary_IC.data[0])
@@ -124,14 +132,17 @@ def built_in_image(input_IC, binary_IC, channel = 2, bit = 1):
         pos_x = 0
         for pixel in row:
             value = pixel[channel]
-            unbited_value = value - int((value%(2**bit))/(2**(bit-1)))
-            bitted_value = unbited_value + int(binary_IC.data[pos_y%binary_height][pos_x%binary_width][0]/255)*(2**(bit-1))
+            unbited_value = value - int((value % (2 ** bit)) / (2 ** (bit - 1)))
+            bitted_value = unbited_value + int(binary_IC.data[pos_y % binary_height][pos_x % binary_width][0] / 255) * (
+                        2 ** (bit - 1))
             output_IC.data[pos_y][pos_x][channel] = bitted_value
+
             pos_x += 1
         pos_y += 1
     return output_IC
 
-def built_in_text(input_IC, binary_text, channel = 2, bit = 1):
+
+def built_in_text(input_IC, binary_text, channel=2, bit=1):
     output_IC = copy.deepcopy(input_IC)
     binary_length = len(binary_text)
     pos_y = 0
@@ -139,11 +150,16 @@ def built_in_text(input_IC, binary_text, channel = 2, bit = 1):
     for row in input_IC.data:
         pos_x = 0
         for pixel in row:
-            value = pixel[channel]
-            unbited_value = value - int((value % (2 ** bit)) / (2 ** (bit - 1)))
-            bit_to_be_bitted = int(binary_text[(pos_y*width+pos_x)%binary_length])
-            bitted_value = unbited_value + bit_to_be_bitted * (2 ** (bit - 1))
-            output_IC.data[pos_y][pos_x][channel] = bitted_value
+            if (pos_y * width + pos_x) < binary_length:
+                value = pixel[channel]
+                unbited_value = value - int((value % (2 ** bit)) / (2 ** (bit - 1)))
+                bit_to_be_bitted = int(binary_text[(pos_y * width + pos_x) % binary_length])
+                bitted_value = unbited_value + bit_to_be_bitted * (2 ** (bit - 1))
+                output_IC.data[pos_y][pos_x][channel] = bitted_value
+            else:
+                value = pixel[channel]
+                unbited_value = value - int((value % (2 ** bit)) / (2 ** (bit - 1)))
+                output_IC.data[pos_y][pos_x][channel] = unbited_value
             pos_x += 1
         pos_y += 1
     return output_IC
--- lab_6/main.py
+++ lab_6/main.py
@@ -1,19 +1,20 @@
 import ImageContainer as IC
 
 while True:
-    #container = IC.ImageContainer(input("relative path of the container: "))
-    #watermark = IC.ImageContainer(input("relative path of the watermark: "))
-    container = IC.ImageContainer("img/InputImage.png")
-    watermark = IC.ImageContainer("img/WM.png")
+    container = IC.ImageContainer(input("relative path of the container: "))
+    watermark = IC.ImageContainer(input("relative path of the watermark: "))
     text = input(">>>")
 
     watermark_mono = IC.IC_to_monochrome(watermark)
     watermark_binary = IC.monochrome_to_binary(watermark_mono)
 
-    result_1 = IC.built_in_image(container, watermark)
+    result_1 = IC.built_in_image(container, watermark_binary)
 
-    # result = IC.built_in_text(, text, 2, 2)
+    result = IC.built_in_text(result_1, text, 1, bit = 1)
 
-    watermark_binary.show()
     IC.monochrome_binary_slice(result_1).show()
+    IC.monochrome_binary_slice(result, channel=1, bit = 1).show()
+    result.show()
+    result.path = "output/result.png"
+    result.save()
     break
